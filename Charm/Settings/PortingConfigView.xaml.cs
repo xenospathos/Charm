2f2e30972bff9e0533b2c935b55c2a176e5322e1
EasterSupper <jack1998@live.com.au>
2026-04-07 20:03:50 +0800

    export ok

--- Charm/Settings/PortingConfigView.xaml.cs
+++ Charm/Settings/PortingConfigView.xaml.cs
@@ -106,10 +106,7 @@ public partial class PortingConfigView : UserControl
             return;
         }
         _config.SetUnrealInteropEnabled(!_config.GetUnrealInteropEnabled());
-        if (_config.GetUnrealInteropEnabled())
-        {
-            _config.SetIndvidualStaticsEnabled(true);
-        }
+        // Individual statics export is now the default behavior in 3.2.3
         PopulateUEConfigPanel();
     }
 }
