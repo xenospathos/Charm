5a7851816852a7a0f69a5fbf8d5bcbd7f285be21
EasterSupper <jack1998@live.com.au>
2026-04-08 17:16:42 +0800

    remove mats from _cfg and parse mats json files

--- Tiger/Exporters/MetadataExporter.cs
+++ Tiger/Exporters/MetadataExporter.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using Newtonsoft.Json;
 using Tiger.Schema;
-using Tiger.Schema.Shaders;
 
 namespace Tiger.Exporters;
 
@@ -33,8 +32,6 @@ class MetadataScene
 
     public MetadataScene(ExporterScene scene, Exporter.ExportEventArgs args)
     {
-        ConcurrentDictionary<string, JsonMaterial> materials = new();
-        _config.TryAdd("Materials", materials);
         ConcurrentDictionary<string, JsonPart> parts = new();
         _config.TryAdd("Parts", parts);
         ConcurrentDictionary<string, ConcurrentBag<JsonInstance>> instances = new();
@@ -68,8 +65,6 @@ class MetadataScene
         {
             foreach (ExporterPart part in mesh.Parts)
             {
-                if (part.Material != null)
-                    AddMaterial(part.Material);
                 AddPart(part, part.Name);
             }
         }
@@ -78,8 +73,6 @@ class MetadataScene
         {
             foreach (ExporterPart part in mesh.Parts)
             {
-                if (part.Material != null)
-                    AddMaterial(part.Material);
                 AddPart(part, part.Name);
             }
         }
@@ -97,8 +90,6 @@ class MetadataScene
         {
             foreach (ExporterPart part in entityMesh.Mesh.Parts)
             {
-                if (part.Material != null)
-                    AddMaterial(part.Material);
                 AddPart(part, part.Name, new MapTransform()
                 {
                     Translation = entityMesh.TranslationOffset,
@@ -137,36 +128,6 @@ class MetadataScene
         }
 
         _config["Parts"][part.SubName].PartMaterials[partName] = part.Material?.Hash ?? "";
-    }
-
-    public void AddMaterial(Material material)
-    {
-        if (!material.Hash.IsValid())
-            return;
-
-        var matInfo = new JsonMaterial
-        {
-            Textures = new Dictionary<string, Dictionary<int, TexInfo>>()
-        };
-
-        if (!_config["Materials"].TryAdd(material.Hash.ToString(), matInfo))
-            return;
-
-        Dictionary<int, TexInfo> vstex = new();
-        matInfo.Textures.Add("VS", vstex);
-        foreach (STextureTag vst in material.Vertex.EnumerateTextures())
-        {
-            if (vst.Texture != null)
-                vstex.TryAdd((int)vst.TextureIndex, new TexInfo { Hash = vst.Texture.Hash, SRGB = vst.Texture.IsSrgb(), Dimension = EnumExtensions.GetEnumDescription(vst.Texture.GetDimension()) });
-        }
-
-        Dictionary<int, TexInfo> pstex = new();
-        matInfo.Textures.Add("PS", pstex);
-        foreach (STextureTag pst in material.Pixel.EnumerateTextures())
-        {
-            if (pst.Texture != null)
-                pstex.TryAdd((int)pst.TextureIndex, new TexInfo { Hash = pst.Texture.Hash, SRGB = pst.Texture.IsSrgb(), Dimension = EnumExtensions.GetEnumDescription(pst.Texture.GetDimension()) });
-        }
     }
 
     public void SetType(ExportType type, DataExportType datatype)
@@ -273,21 +234,4 @@ class MetadataScene
         public float[] Corner2;
     }
 
-    private struct JsonMaterial
-    {
-        public JsonMaterial()
-        {
-        }
-
-        public bool BackfaceCulling { get; set; } = true;
-        public List<string> UsedScopes;
-        public Dictionary<string, Dictionary<int, TexInfo>> Textures;
-    }
-
-    public struct TexInfo
-    {
-        public string Hash { get; set; }
-        public string Dimension { get; set; }
-        public bool SRGB { get; set; }
-    }
 }
